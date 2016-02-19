fa206bdf5dc6153b1a56d5fe08b182e1728c023a
human.of.rain <hauman.of.rain@gmail.com>
2016-02-19 14:22:36 +0300

    Final

--- app/controllers/recipes_controller_controller.rb
+++ app/controllers/recipes_controller_controller.rb
@@ -1,6 +1,6 @@
 class RecipesControllerController < ApplicationController
   def index
   	@search_team = params[:search] || 'chocolate'
-  	@courses = Recipe.for(@search_team)
+  	@Recipes = Recipe.for(@search_team)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,20 @@
-class Recipe < ActiveRecord::Base
+require 'httparty'
+ENV["FOOD2FORK_KEY"] = 'b73e80acf4e1e0dd17d6daa82605bf22'
+class Recipe
+  include HTTParty
+  
+  default_params key: ENV["FOOD2FORK_KEY"]
+  hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || "www.food2fork.com"
+  base_uri "http://#{hostport}/api"
+ 
+  format :json
+
+  key_value = ENV["FOOD2FORK_KEY"]
+
+
+
+
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
 end
